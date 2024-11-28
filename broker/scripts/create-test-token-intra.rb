b024fc0c3f343d5aeb152626c34bdf1476ff42aa
elkezza <salih.elkezza@gmail.com>
2024-11-28 11:10:47 +0000

    adding the missing attributes.

--- broker/scripts/create-test-token-intra.rb
+++ broker/scripts/create-test-token-intra.rb
@@ -15,7 +15,12 @@ payload = {
   'exp' => Time.new.to_i + 3600,
   'nbf' => Time.new.to_i,
   'iat' => Time.new.to_i,
-  'aud' => 'idsc:IDS_CONNECTORS_ALL'
+  'aud' => 'idsc:IDS_CONNECTORS_ALL',
+  'referringConnector' => 'http://broker.demo',
+  '@type' => 'ids:DatPayload',
+  'securityProfile' => 'idsc:BASE_SECURITY_PROFILE',
+  '"@context"'  => 'https://w3id.org/idsa/contexts/context.jsonld',
+  'https://w3id.org/idsa/contexts/context.jsonld' => '391273c2c98c03cb37637b8e38fa6187276309564f1a8a40e093c01e419ad031'
 }
 
 token = JWT.encode payload, client_rsa_key, 'RS256'
